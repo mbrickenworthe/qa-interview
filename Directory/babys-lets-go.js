d530144c2ad3a2cbe0781253646d95069a42496f
mbrickenworthe <adamwinters456@gmail.com>
2022-05-06 15:57:37 -0400

    Adding the files

---
+++ Directory/babys-lets-go.js
@@ -0,0 +1,9 @@
+// Spring is in the air, with new life abounding.
+
+// I've found information about baby names. (Presumabely in the US...)
+
+// I've found a list of baby names going back more than a century.
+
+// What was the most popular baby name (boy and girl) every year?
+
+// What were the most androgynous names (near 50% girl and 50% boy)?
